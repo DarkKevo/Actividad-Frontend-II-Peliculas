b52ea98ae5c5cca23e190bc31d4202703a6ecd88
DarkKevo <kevinaraujogonzalez@gmail.com>
2023-03-13 00:08:37 -0400

    Agregada la ruta de obtener peliculas y documentada esta misma

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,10 +1,34 @@
 import Express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { routes } from './routes/routes.js'
+import { routes } from './routes/routes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//Swagger API
+import SwaggerUI from 'swagger-ui-express';
+import SwaggerJsDoc from 'swagger-jsdoc';
+
+const swagerSpect = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Api-Peliculas',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: [`${path.join(__dirname, './routes/routes.js')}`],
+};
 
 //Deployment
-import './controllers/Deploy/Deploy.js'
+import './controllers/Deploy/Deploy.js';
 
 const app = Express();
 
@@ -16,11 +40,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', routes);
+app.use('/Documentation', SwaggerUI.serve, SwaggerUI.setup(SwaggerJsDoc(swagerSpect)));
 
 //APP set
 app.set('port', process.env.PORT || port);
 
 app.listen(app.get('port'), () => {
-    console.log(`[Running] - PORT: ${port}`);
-})
-
+  console.log(`[Running] - PORT: ${port}`);
+});
--- Server/src/routes/routes.js
+++ Server/src/routes/routes.js
@@ -1,7 +1,82 @@
 import Express from 'express';
+import { GetMovies } from '../controllers/GET/GetMovies.js';
 
 export const routes = Express.Router();
 
-routes.get('/', (req, res) => {
-    res.send('Hello World')
-})
+//Documentacion de Schema de Movies
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Movies:
+ *       type: object
+ *       properties:
+ *         Genero:
+ *           type: string
+ *           description: Genero de la Pelicula
+ *         Titulo:
+ *           type: string
+ *           description: Titulo de la Pelicula
+ *         Sinopsis:
+ *           type: number
+ *           description: Contexto de la Pelicula
+ *         Imagen:
+ *           type: date
+ *           description: Nombre de la Imagen
+ *         Fecha_Publicacion:
+ *           type: string
+ *           description: Fecha de Publicacion de la Pelicula
+ *         Actores_Principales:
+ *           type: string
+ *           description: Actores Principales de la Pelicula
+ *         Directores:
+ *           type: string
+ *           description: Directores de la Pelicula
+ *         Franquicia:
+ *           type: string
+ *           description: Franquicia de la Pelicula
+ *         Review:
+ *           type: json
+ *           description: Opiniones de la Pelicula
+ *       required:
+ *         -Genero
+ *         -Titulo
+ *         -Sinopsis
+ *         -Imagen
+ *         -Fecha_Publicacion
+ *         -Actores_Principales
+ *         -Directores
+ *         -Franquicia
+ *         -Review
+ *       example:
+ *         Genero: 1
+ *         Titulo: El Titanic
+ *         Sinopsis: LA historia del hundimiento de un barco
+ *         Fecha_Publicacion: 2004/02/07
+ *         Actores_Principales: Leonardo DiCaprio
+ *         Directores: Antonio Banderas
+ *         Franquicia: Disney
+ *         Review: [{username: "John", comment: "A beautifull movie"}]
+ */
+
+//Documentacion GetMovies
+/**
+ * @swagger
+ * /GetMovies:
+ *   get:
+ *     summary: Retornar todas las peliculas
+ *     tags: [Movies]
+ *     responses:
+ *       200:
+ *         description: Todos las Peliculas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Movies'
+ *           
+ */
+routes.get('/GetMovies', GetMovies, (req, res) => {
+    //Obtencion de Peliculas
+});
